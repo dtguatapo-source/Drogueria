4ab84050d069c4d7d7ca4e92fe8321a7c5dddd02
derlly tatiana guatapo <dt.guatapo@unimayor.edu.co>
2026-03-19 13:18:46 -0500

    se sube login con validacione e imagenes avances

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +9,11 @@ import { RouterLink } from '@angular/router';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+
+ constructor(private router: Router) {}
+
+  salir(){
+    this.router.navigate(['/']);
+  }
+}
