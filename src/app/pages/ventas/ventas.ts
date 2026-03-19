d90a648290f4b5c95696e2351c0d95f013a459d5
Camila Quinones <camiqg2905@gmail.com>
2026-03-19 17:28:53 -0500

    modificaciones

--- src/app/pages/ventas/ventas.ts
+++ src/app/pages/ventas/ventas.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ventas',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
   templateUrl: './ventas.html',
   styleUrl: './ventas.css',
 })
-export class Ventas {}
+export class Ventas {
+
+  
+}
