471710ab8b67559a3db88d03e6ce2cb2e182a557
Camila Quinones <camiqg2905@gmail.com>
2026-03-26 15:05:49 -0500

    nuevas modificaciones

--- src/app/pages/informes/informes.ts
+++ src/app/pages/informes/informes.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-informes',
   standalone:true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './informes.html',
   styleUrl: './informes.css',
 })
-export class Informes {}
+export class Informes {
+
+  FormularioInformes: FormGroup;
+  constructor(private form: FormBuilder, private router: Router) {
+    this.FormularioInformes = this.form.group({
+
+      fechaInicial: ['', Validators.required],
+      fechaFinal: ['', Validators.required]
+    })
+}
+
+buscarVentas() {
+    if (this.FormularioInformes.invalid) {
+      console.log('Formulario inválido');
+      return;
+    }
+     const { fechaInicial, fechaFinal } = this.FormularioInformes.value;
+
+    console.log('Buscando entre:', fechaInicial, fechaFinal);
+  }
+   exportarExcel() {
+    console.log('Exportando a Excel...');
+  }
+  
+  irAdmin() {
+    this.router.navigate(['/admin']);
+  }
+
+  salir() {
+    console.log('Cerrando sesión...');
+    this.router.navigate(['/login']);
+  }
+
+  volverInicio() {
+    this.router.navigate(['/']);
+  }
+}
--- src/app/pages/ventas/ventas.ts
+++ src/app/pages/ventas/ventas.ts
@@ -1,14 +1,76 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
-  standalone:true,
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './ventas.html',
   styleUrl: './ventas.css',
 })
 export class Ventas {
 
-  
+  FormularioVentas: FormGroup;
+  detalleVenta: any[] = [];
+
+  constructor(private form: FormBuilder, private router: Router) {
+
+    this.FormularioVentas = this.form.group({
+      fechaCaducidad: ['', Validators.required],
+      fechaVenta: ['', Validators.required],
+      Cliente: ['', Validators.required],
+      Factura: ['', Validators.required],
+      Medicamento: ['', Validators.required],
+      Cantidad: ['', Validators.required],
+      Codigo: ['', Validators.required],
+      Precio: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+      Laboratorio: ['', Validators.required],
+      Total: ['']
+    });
+
+  }
+
+  agregarProducto() {
+
+    if (this.FormularioVentas.invalid) {
+      console.log('Formulario inválido');
+      return;
+    }
+
+    const producto = this.FormularioVentas.value;
+
+    producto.Total = producto.Cantidad * producto.Precio;
+
+    this.detalleVenta.push(producto);
+
+    console.log('Producto agregado:', producto);
+  }
+
+  quitarProducto() {
+    this.detalleVenta.pop();
+  }
+
+  confirmarVenta() {
+    console.log('Venta confirmada:', this.detalleVenta);
+  }
+
+  imprimirVenta() {
+    window.print();
+  }
+
+  limpiarFormulario() {
+    this.FormularioVentas.reset();
+    this.detalleVenta = [];
+  }
+
+  irAdministrador() {
+    this.router.navigate(['/admin']);
+  }
+
+  salir() {
+    this.router.navigate(['/login']);
+  }
+
 }
